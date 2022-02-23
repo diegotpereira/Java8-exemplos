4b3483fbd7088766c7a58624d9fee9b8ac76ca5d
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-02-23 15:54:07 -0300

    first commit

---
+++ src/br/com/java/objetos/OlaData.java
@@ -0,0 +1,12 @@
+package br.com.java.objetos;
+
+import java.util.Date;
+
+public class OlaData {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Olá, hoje é: ");
+		System.out.println(new Date());
+	}
+}
