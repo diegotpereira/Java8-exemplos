842083c52566d12a822b8f4021fc50b54d2553da
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-03-01 08:35:44 -0300

    alteração na classe formas.

--- src/br/com/java/poliformismo/forma/Formas.java
+++ src/br/com/java/poliformismo/forma/Formas.java
@@ -5,7 +5,7 @@ public class Formas {
 	public static void main(String[] args) {
 		FormasAleatorias gen = new FormasAleatorias();
 
-		for(Forma forma : gen.array(0))
+		for(Forma forma : gen.array(9))
 		forma.desenhar();
 	}
 }
